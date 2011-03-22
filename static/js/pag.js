f1426c2fa69d01bb4935356c59913ad3d588d022
Christopher Sanders <astericky@gmail.com>
2011-03-22 13:58:00 +0000

    application iframe updates.

--- static/js/pag.js
+++ static/js/pag.js
@@ -6,9 +6,9 @@ $('.petPag').click(function(){
 	$.ajax({
 		url: address,
 		success: function(results){
-			$('.petPag > img').fadeOut();
+			$('.petPag > img').fadeOut(100);
 			$('.petPag').html('<a href="#">See More Pets!</a>').hide().fadeIn(400);
-			$('#availablePets').append(results).stop().hide().slideDown(400);
+			$('#availablePets').append(results).hide().stop().slideDown(400);
 		}
 	});	
 	return false;

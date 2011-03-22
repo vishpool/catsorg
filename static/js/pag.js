5e23083f14b1ef9b10b5f7a9d5dde67357a78763
Christopher Sanders <astericky@gmail.com>
2011-03-22 00:51:17 +0000

    adoptions pagination javascript! changes and css changes

--- static/js/pag.js
+++ static/js/pag.js
@@ -1,10 +1,14 @@
-$('.petPag > a').click(function(){
+$('.petPag').click(function(){
+	$('.petPag > a').fadeOut();
+	$('.petPag').html('<img src="images/ajax-loader.gif" alt="Loading..." />');
 	var offset = $('.pet').length;
 	var address = '/svc/adoptions/' + offset + '.html';
 	$.ajax({
 		url: address,
 		success: function(results){
-			 $('#availablePets').append(results).hide().fadeIn(400);
+			$('.petPag > img').fadeOut();
+			$('.petPag').html('<a href="#">See More Pets!</a>').hide().fadeIn(400);
+			$('#availablePets').append(results).stop().hide().slideDown(400);
 		}
 	});	
 	return false;
